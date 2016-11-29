7d2f1df6b8ffe05fd86e0d6607188b9d1da8d9d3
George Hopkins <george-hopkins@null.net>
2016-11-29 19:10:13 +0100

    Set default prefix

--- src/opentom_skel/bin/navit.sh
+++ src/opentom_skel/bin/navit.sh
@@ -5,16 +5,11 @@ ls $DIST/bin/gltt || {
 	exit
 }
 
-export LANG=fr_FR.utf8
-
-export NAVIT_PREFIX=$DIST
-export NAVIT_LIBDIR=$DIST/lib/navit
-export NAVIT_SHAREDIR=$DIST/share/navit
+export LANG=en_US.utf8
 
 if [ -z `pidof gltt` ]; then rc.gltt start; fi
 echo EnableRawGPSOutput >/dev/gps
 
-navit -c $NAVIT_SHAREDIR/navit$NANOX_YRES.xml $* >$DIST/logs/navit.log 2>&1
+navit -c $DIST/share/navit/navit.xml $* >$DIST/logs/navit.log 2>&1
+
 rc.gltt stop
-
-
