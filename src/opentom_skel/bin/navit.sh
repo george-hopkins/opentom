607e73ab585fd7c9f19767cc7fa1e9bf5029c8b9
George Hopkins <george-hopkins@null.net>
2016-11-29 21:13:21 +0100

    Build configuration for 480x272

--- src/opentom_skel/bin/navit.sh
+++ src/opentom_skel/bin/navit.sh
@@ -10,6 +10,12 @@ export LANG=en_US.utf8
 if [ -z `pidof gltt` ]; then rc.gltt start; fi
 echo EnableRawGPSOutput >/dev/gps
 
-navit -c $DIST/share/navit/navit.xml $* >$DIST/logs/navit.log 2>&1
+if [ "`cat /proc/barcelona/tfttype`" == 2 ] || [ "`cat /proc/barcelona/tfttype`" == 3 ] ; then
+	NAVIT_CONFIG=$DIST/share/navit/navit.xml
+else
+	NAVIT_CONFIG=$DIST/share/navit/navit_xl.xml
+fi
+
+navit -c $NAVIT_CONFIG $* >$DIST/logs/navit.log 2>&1
 
 rc.gltt stop
